67f1674c28ffe43d2963be66a70cdfdeeedae060
leopoldo <ll11ll1@outlook.es>
2022-01-28 10:04:37 +0000

    delete docker dependency, silence tensorflow, production state

--- src/evaluater/predict.py
+++ src/evaluater/predict.py
@@ -1,5 +1,8 @@
 
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # silence tensorflow
 import glob
 import json
 import argparse
